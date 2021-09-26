76bbbe88324633bb08b2d81acd35e76fa49636e1
Stephen <stephenolatunji02@gmail.com>
2021-09-26 10:30:10 +0100

    removed env

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'https://app.anelloh.com:44312',
-  url2: 'https://app.anelloh.com:44310',
-  stripeToken: 'pk_test_51I7HWoK1bNuoSa5XZ4MxHbaVMi0zeDfMbiwxuzsfmLK8DSo2krWds3ng6D6Z5ci1skQFOjL4NXfGCn4mL5YJAcMX00MxzQyEeI',
-  payStackToken: 'sk_test_3cfffb1a48e923de96492fc2e1ac46043e3de240',
-  app_url: 'https://app.anelloh.com:44313'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  url: 'https://app.anelloh.com:44312',
-  url2: 'https://app.anelloh.com:44310',
-  stripeToken: 'pk_test_51I7HWoK1bNuoSa5XZ4MxHbaVMi0zeDfMbiwxuzsfmLK8DSo2krWds3ng6D6Z5ci1skQFOjL4NXfGCn4mL5YJAcMX00MxzQyEeI',
-  payStackToken: 'sk_test_3cfffb1a48e923de96492fc2e1ac46043e3de240',
-  app_url: 'https://localhost:4200'
-};
 
 /*
  * For easier debugging in development mode, you can import the following file
